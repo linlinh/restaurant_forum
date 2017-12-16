b087d293f684d4885631c9d3646ebe866d6716e1
cortez528 <cortez528@gmail.com>
2017-12-16 21:16:40 +0800

    Revert "wrong spelling for categories controller"
    This reverts commit e4240a1d33404c1c3eb5b79559a90bbd62e30ef6.

--- app/controllers/admin/cagegories_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::CagegoriesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :authenticate_admin
-
-  def index
-    @categories = Category.all 
-  end
-  
-end
--- app/helpers/admin/cagegories_helper.rb
+++
@@ -1,2 +0,0 @@
-module Admin::CagegoriesHelper
-end
